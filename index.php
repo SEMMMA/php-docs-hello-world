9f362704844b217d19398bd974638b33082e2291
SEMMMA <77170866+SEMMMA@users.noreply.github.com>
2021-02-11 23:48:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello World!";
+echo "<center><h1>Salah Mosbah POC!<h1></center>";
+
+echo "<center><h1>Subdomain TakeOver is Here<h1></center>";
+
+echo "<script>alert('Xss Vulnerabilities')</script>";
+
+?>
+
+
